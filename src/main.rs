a1da213453db55127fa93c0751d6bea889cb9d9a
Kushagra <sparrow.kushagra@gmail.com>
2025-03-07 13:12:27 +0530

    feat: add query params support on /params endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct QueryParams {
+    name: Option<String>,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -8,6 +14,15 @@ async fn hello() -> impl Responder {
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+#[get("params")]
+async fn params(query: web::Query<QueryParams>) -> impl Responder {
+    if let Some(value) = &query.name {
+        HttpResponse::Ok().body(format!("name: {}", value))
+    } else {
+        HttpResponse::Ok().body("params not found")
+    }
 }
 
 async fn mannual_hello() -> impl Responder {
@@ -20,6 +35,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(params)
             .route("/hey", web::get().to(mannual_hello))
     })
     .bind(("127.0.0.1", 8000))?
