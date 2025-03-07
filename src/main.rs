543c1c29a12cb835754d6ec4c637994f853e08d7
Kushagra <sparrow.kushagra@gmail.com>
2025-03-07 13:20:03 +0530

    fix(src/main): fix the ip

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ async fn main() -> std::io::Result<()> {
             .service(params)
             .route("/hey", web::get().to(mannual_hello))
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
